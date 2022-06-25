29bd315a46f334a976a83fc62fd0f98e706df50b
SnapdragonLee <lixiaolong8088888@gmail.com>
2022-06-25 18:38:41 +0800

    init repo for BUAA 2021 OS

---
+++ OS_ENV/compiler/README.md
@@ -0,0 +1,2 @@
+
+### THis is a copy from ELDK 4.1, but have no license yet.
---
+++ aTestGxemul.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#run gxemul
+
+export PROJECT_DIR="/home/snapdragon_manjaro21/Project_Manjr/C_Based/OS_OASIS/OS_ENV/"
+
+make
+
+OS_ENV/gxemul -E testmips -C R3000 -M 64 -d gxemul/fs.img gxemul/vmlinux
+
+make clean
